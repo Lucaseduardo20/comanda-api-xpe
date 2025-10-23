df94a8381afc81f28893eb27d25a90483ec520c2
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2025-10-23 19:41:29 -0300

    feature: implementing client repository and jpa config

---
+++ src/main/java/comanda/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.lucas.comanda.repository;
+
+import com.lucas.comanda.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByNome(String nome);
+}
